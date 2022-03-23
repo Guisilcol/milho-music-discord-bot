f36be9ffcdf637fe6e5ba71cf543c0cf58b5dec1
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2022-03-22 21:39:04 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const {createMapQueue: createMapServersQueue} = require("./modules/Queue");
 const serversQueues = createMapServersQueue();
 const client = new Discord.Client();
 
+console.log("Segue o objeto: ");
+console.log(config);
+
 setupDiscordClient(client, config, serversQueues);
 
 client.login(config.token);
