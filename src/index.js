031392c910d766d1ab6b9ab2c278531b6ed18822
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2022-03-22 22:00:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const setupDiscordClient = require("./controller");
 const config = require("./config.js");
 const {createMapQueue: createMapServersQueue} = require("./modules/Queue");
+const http = require("http");
 
 const serversQueues = createMapServersQueue();
 const client = new Discord.Client();
@@ -9,3 +10,5 @@ const client = new Discord.Client();
 setupDiscordClient(client, config, serversQueues);
 
 client.login(config.token);
+
+http.createServer(() => console.log("> Heroku: ping feito com sucesso")).listen(process.env.PORT || 6000);
