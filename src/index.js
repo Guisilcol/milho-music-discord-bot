d6d4bcbe2f9898435ed5345c8e8db4d9504aa474
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2022-04-03 19:50:45 -0300

    Update index.js
    Adição do comentário especificando a rota do Heroku

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
+const http = require("http");
 const setupDiscordClient = require("./controller");
 const config = require("./config.js");
 const {createMapQueue: createMapServersQueue} = require("./modules/Queue");
-const http = require("http");
 
 const serversQueues = createMapServersQueue();
+
 const client = new Discord.Client();
 
 setupDiscordClient(client, config, serversQueues);
-
 client.login(config.token);
 
+//Criação de um endpoint pro Heroku, para que não haja problemas ao acessar o host do bot
 http.createServer((_, response) => response.end())
     .listen(process.env.PORT || 6000);
